b79eb3f7fc580365baa49dba7479897b96810087
Blain Crawford <blaincrawford@gmail.com>
2022-02-09 15:19:57 -0500

    page header in progress starting page changing logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,68 @@
+import './styles.css';
 
-let mainContent = (() => {
-  const createHeader = function () {
-    let headerDiv = document.createElement('div');
-    let pageContent = document.getElementById('content');
-    headerDiv.innerText = 'Hello, World!!';
-    content.appendChild(headerDiv);
-  }
+const headerContent = (() => {
+  const createHeader = (() => {
+    const headerDiv = document.createElement('div');
+    const siteTitle = document.createElement('h1');
+    const pageContent = document.getElementById('content');
+    const navBar = document.createElement('ul');
+    const home = document.createElement('li');
+    const recipes = document.createElement('li');
+    const contact = document.createElement('li');
 
-  return{createHeader}
+    siteTitle.innerText = 'Blain Crawford\'s Home Recipes';
+    home.innerText = 'Home';
+    recipes.innerText = 'Recipes';
+    contact.innerText = 'Contact';
+
+    headerDiv.classList = 'header';
+    navBar.classList = 'navbar';
+
+    headerDiv.appendChild(siteTitle);
+    headerDiv.appendChild(navBar);
+    navBar.appendChild(home);
+    navBar.appendChild(recipes);
+    navBar.appendChild(contact);
+    pageContent.appendChild(headerDiv);
+  })();
 })();
 
-mainContent.createHeader();
+const mainContent = function () {
+  let pageBody = document.createElement('div')
+  document.body.appendChild(pageBody);
+
+  let homePage = function () {
+    pageBody.innerHTML = ''
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Home Page!';
+    pageBody.appendChild(h1)
+  };
+  let recipesPage = function () {
+    pageBody.innerHTML = ''
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Recipes Page!';
+    pageBody.appendChild(h1)
+  };
+  let contactPage = function () {
+    pageBody.innerHTML = ''
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Contact Page!';
+    pageBody.appendChild(h1)
+  };
+  return {homePage, recipesPage, contactPage};
+};
+
+const currentPage = mainContent();
+const links = document.querySelectorAll('li');
+
+const switchPages = function () {
+  links.forEach((link) => {
+    link.classList.remove('current-page');
+  });
+  this.classList.add('current-page');
+  currentPage.homePage();
+};
+
+links.forEach((link) => {
+  link.addEventListener('click', switchPages.bind(link));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: "source-map",
   devServer: {
     hot: 'only',
     static: {
@@ -19,6 +20,10 @@ module.exports = {
       {
         test: /\.js$/i,
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
