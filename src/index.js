889040dd17c1b0dee0ca4b55e8eb17a43ec8948c
Blain Crawford <blaincrawford@gmail.com>
2022-02-10 17:41:53 -0500

    logic for individual recipe pages working

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import Chrain from './images/chrain.jpg';
 import BagelAvocado from './images/bagel_avocado.jpg';
 import Gumbo from './images/gumbo.jpg';
 import ShortRibs from './images/shortribs.jpg';
-import Pie from './images/pie.jpg'
+import Pie from './images/pie.jpg';
 
 class Page {
-  constructor (bodyTopper, contentWrapper, pageClass, images) {
+  constructor(bodyTopper, contentWrapper, pageClass, images) {
     this.bodyTopper = bodyTopper;
     this.contentWrapper = contentWrapper;
     this.contentWrapper.classList = pageClass;
     this.images = images;
-  };
+  }
 
-  homePageBody () {
+  homePageBody() {
     this.contentWrapper.innerHTML = '';
 
     const pageTitle = document.createElement('h2');
     const cooking = document.createElement('h3');
-    cooking.innerHTML = 'Click on Recipes to get cooking!'
+    cooking.innerHTML = 'Click on Recipes to get cooking!';
     const headerImage = new Image();
 
     pageTitle.textContent = this.bodyTopper;
@@ -29,9 +29,9 @@ class Page {
     this.contentWrapper.appendChild(pageTitle);
     this.contentWrapper.appendChild(cooking);
     this.contentWrapper.appendChild(headerImage);
-  };
+  }
 
-  contactPageBody () {
+  contactPageBody() {
     this.contentWrapper.innerHTML = '';
 
     const pageTitle = document.createElement('h2');
@@ -40,25 +40,25 @@ class Page {
     const headerImage = new Image();
 
     pageTitle.textContent = this.bodyTopper;
-    intro.textContent = 'Blain and Christina (Chrain) have been living together for nearly three years now.  Both are computer programmers by day, but at night, and on weekends, they specialize as cooks with many different specialties.  When not at work you can find them out seeing live music, playing video games with friends, or in the kitchen making the delicious food on this site, and much more when they aren\'t writing music or programs!';
+    intro.textContent =
+      'Blain and Christina (Chrain) have been living together for nearly three years now.  Both are computer programmers by day, but at night, and on weekends, they specialize as cooks with many different specialties.  When not at work you can find them out seeing live music, playing video games with friends, or in the kitchen making the delicious food on this site, and much more when they aren\'t writing music or programs!';
     headerImage.src = this.images;
     headerImage.classList = 'home-image';
-    introContainer.classList = 'intro-container'
-    
+    introContainer.classList = 'intro-container';
+
     introContainer.appendChild(intro);
     this.contentWrapper.appendChild(pageTitle);
     this.contentWrapper.appendChild(headerImage);
     this.contentWrapper.appendChild(introContainer);
-  };
+  }
 
-  recipesPageBody () {
+  recipesPageBody() {
     const recipeList = this.images;
     this.contentWrapper.innerHTML = '';
 
     const pageTitle = document.createElement('h2');
     const cooking = document.createElement('h3');
-    cooking.innerHTML = 'Click on pictures for detailed recipe!'
-    
+    cooking.innerHTML = 'Click on pictures for detailed recipe!';
 
     pageTitle.textContent = this.bodyTopper;
 
@@ -66,10 +66,10 @@ class Page {
     this.contentWrapper.appendChild(cooking);
     let recipesDiv = document.createElement('div');
 
-    for(let i = 0; i < recipeList.length; i++) {
-      let individualRecipe = document.createElement('div')
-      let recipeName = document.createElement('h3')
-      let recipePic = new Image()
+    for (let i = 0; i < recipeList.length; i++) {
+      let individualRecipe = document.createElement('div');
+      let recipeName = document.createElement('h3');
+      let recipePic = new Image();
 
       recipesDiv.classList = 'recipe';
       recipePic.classList = 'recipe-image';
@@ -81,43 +81,56 @@ class Page {
 
       individualRecipe.appendChild(recipeName);
       individualRecipe.appendChild(recipePic);
-      recipesDiv.appendChild(individualRecipe)
+      recipesDiv.appendChild(individualRecipe);
       this.contentWrapper.appendChild(recipesDiv);
     }
-
-  };
-};
+  }
+}
 
 const displayRecipePage = function (page) {
   page = this.id;
   let pageBody = document.querySelector('.recipe-page');
+  let instructionsHeader = document.createElement('h2');
+  let ingredientList = document.createElement('div');
   switch (page) {
     case '1':
-    pageBody.innerHTML = '';
-    pageBody.innerHTML = `<h1>Smoked Salmon Bagel and Avocado!</h1><img src=${BagelAvocado} class='recipe-image'>`;
-    pageBody.appendChild(returnToRecipes);
-    pageBody.classList = 'recipe-page';
-    break;
+      pageBody.innerHTML = '';
+      pageBody.innerHTML = `<h1>Smoked Salmon Bagel and Avocado!</h1><img src=${BagelAvocado} class='recipe-image'>`;
+      pageBody.classList = 'recipe-page';
+      instructionsHeader.innerText = 'Ingredients'
+      ingredientList.innerHTML = '<ul><li>Bagel</li><li>Smoked Salmon</li><li>Avocado</li><li>Pickeled Onions and Jalepenós</li><li>Hot Sauce</li></ul>'
+      pageBody.appendChild(instructionsHeader);
+      pageBody.appendChild(ingredientList);
+      break;
     case '2':
-    pageBody.innerHTML = '';
-    pageBody.innerHTML = `<h1>Gumbo</h1><br><img src=${Gumbo} class='recipe-image'>`;
-    pageBody.appendChild(returnToRecipes);
-    pageBody.classList = 'recipe-page';
-    break;
+      pageBody.innerHTML = '';
+      pageBody.innerHTML = `<h1>Gumbo</h1><br><img src=${Gumbo} class='recipe-image'>`;
+      pageBody.classList = 'recipe-page';
+      instructionsHeader.innerText = 'Ingredients';
+      ingredientList.innerHTML = '<ul><li>Bagel</li><li>Smoked Salmon</li><li>Avocado</li><li>Pickeled Onions and Jalepenós</li><li>Hot Sauce</li></ul>'
+      pageBody.appendChild(instructionsHeader);
+      pageBody.appendChild(ingredientList);
+      break;
     case '3':
-    pageBody.innerHTML = '';
-    pageBody.innerHTML = `<h1>Pecan Pie!</h1><br><img src=${Pie} class='recipe-image'>`;
-    pageBody.appendChild(returnToRecipes);
-    pageBody.classList = 'recipe-page';
-    break;
+      pageBody.innerHTML = '';
+      pageBody.innerHTML = `<h1>Pecan Pie!</h1><br><img src=${Pie} class='recipe-image'>`;
+      pageBody.classList = 'recipe-page';
+      instructionsHeader.innerText = 'Ingredients';
+      ingredientList.innerHTML = '<ul><li>Bagel</li><li>Smoked Salmon</li><li>Avocado</li><li>Pickeled Onions and Jalepenós</li><li>Hot Sauce</li></ul>'
+      pageBody.appendChild(instructionsHeader);
+      pageBody.appendChild(ingredientList);
+      break;
     case '4':
-    pageBody.innerHTML = '';
-    pageBody.innerHTML = `<h1>Gochujang Short Ribs!</h1><br><img src=${ShortRibs} class='recipe-image'>`;
-    pageBody.appendChild(returnToRecipes);
-    pageBody.classList = 'recipe-page';
-    break;
+      pageBody.innerHTML = '';
+      pageBody.innerHTML = `<h1>Gochujang Short Ribs!</h1><br><img src=${ShortRibs} class='recipe-image'>`;
+      pageBody.classList = 'recipe-page';
+      instructionsHeader.innerText = 'Ingredients';
+      ingredientList.innerHTML = '<ul><li>Bagel</li><li>Smoked Salmon</li><li>Avocado</li><li>Pickeled Onions and Jalepenós</li><li>Hot Sauce</li></ul>'
+      pageBody.appendChild(instructionsHeader);
+      pageBody.appendChild(ingredientList);
+      break;
   }
-}
+};
 
 const headerContent = (() => {
   const createHeader = (() => {
@@ -150,41 +163,58 @@ const headerContent = (() => {
     navBar.appendChild(contact);
     pageContent.appendChild(headerDiv);
   })();
-  let pageBody = document.createElement('div')
+  let pageBody = document.createElement('div');
   document.body.appendChild(pageBody);
-  let homePage = new Page('Welcome to Chrain\'s Home Recipes!', pageBody, 'home-page', Ingredients);
+  let homePage = new Page(
+    'Welcome to Chrain\'s Home Recipes!',
+    pageBody,
+    'home-page',
+    Ingredients
+  );
   homePage.homePageBody();
-
 })();
-
-
 
 const mainContent = function () {
   let pageBody = document.querySelector('.home-page');
   document.body.appendChild(pageBody);
   let recipes = [
-    {name: 'Salmon Bagel with Avocado', picture: BagelAvocado, id: 1},
-    {name: 'Gumbo', picture: Gumbo, id: 2},
-    {name: 'Pecan Pie', picture: Pie, id: 3},
-    {name: 'Gochujang Shortribs', picture: ShortRibs, id: 4}
-  ]
+    { name: 'Salmon Bagel with Avocado', picture: BagelAvocado, id: 1, ingredients: '<ul><li>Bagel</li><li>Smoked Salmon</li><li>Avocado</li><li>Pickeled Onions and Jalepenós</li><li>Hot Sauce</li></ul>' },
+    { name: 'Gumbo', picture: Gumbo, id: 2 },
+    { name: 'Pecan Pie', picture: Pie, id: 3 },
+    { name: 'Gochujang Shortribs', picture: ShortRibs, id: 4 },
+  ];
 
   let homePage = function () {
-    let homePage = new Page('Welcome to Chrain\'s Home Recipes!', pageBody, 'home-page', Ingredients);
+    let homePage = new Page(
+      'Welcome to Chrain\'s Home Recipes!',
+      pageBody,
+      'home-page',
+      Ingredients
+    );
     homePage.homePageBody();
   };
 
   let recipesPage = function () {
-    let contactPage = new Page('The real star of the show. . .the FOOD!', pageBody, 'recipe-page', recipes);
+    let contactPage = new Page(
+      'The real star of the show. . .the FOOD!',
+      pageBody,
+      'recipe-page',
+      recipes
+    );
     contactPage.recipesPageBody();
   };
 
   let contactPage = function () {
-    let contactPage = new Page('Let\'s meet our cooks!!', pageBody, 'home-page', Chrain);
+    let contactPage = new Page(
+      'Let\'s meet our cooks!!',
+      pageBody,
+      'home-page',
+      Chrain
+    );
     contactPage.contactPageBody();
   };
 
-  return {homePage, recipesPage, contactPage};
+  return { homePage, recipesPage, contactPage };
 };
 
 const currentPage = mainContent();
@@ -205,7 +235,7 @@ const switchPages = function () {
     case 'Contact':
       currentPage.contactPage();
       break;
-  };
+  }
 };
 
 links.forEach((link) => {
