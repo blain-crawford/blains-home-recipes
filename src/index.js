a604ab998f0f9cdced9cb99c254697603c182d0f
Blain Crawford <blaincrawford@gmail.com>
2022-02-10 16:13:16 -0500

    got recipe page logic down and working on individual recipes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './styles.css';
-import Ingredients from './images/ingredients.jpg'
-import Chrain from './images/chrain.jpg'
+import Ingredients from './images/ingredients.jpg';
+import Chrain from './images/chrain.jpg';
+import BagelAvocado from './images/bagel_avocado.jpg';
+import Gumbo from './images/gumbo.jpg';
+import ShortRibs from './images/shortribs.jpg';
+import Pie from './images/pie.jpg'
 
 class Page {
   constructor (bodyTopper, contentWrapper, pageClass, images) {
@@ -45,8 +49,71 @@ class Page {
     this.contentWrapper.appendChild(pageTitle);
     this.contentWrapper.appendChild(headerImage);
     this.contentWrapper.appendChild(introContainer);
-  }
+  };
+
+  recipesPageBody () {
+    const recipeList = this.images;
+    this.contentWrapper.innerHTML = '';
+
+    const pageTitle = document.createElement('h2');
+    const cooking = document.createElement('h3');
+    cooking.innerHTML = 'Click on pictures for detailed recipe!'
+    
+
+    pageTitle.textContent = this.bodyTopper;
+
+    this.contentWrapper.appendChild(pageTitle);
+    this.contentWrapper.appendChild(cooking);
+    let recipesDiv = document.createElement('div');
+
+    for(let i = 0; i < recipeList.length; i++) {
+      let individualRecipe = document.createElement('div')
+      let recipeName = document.createElement('h3')
+      let recipePic = new Image()
+
+      recipesDiv.classList = 'recipe';
+      recipePic.classList = 'recipe-image';
+      recipePic.addEventListener('click', displayRecipePage, false);
+
+      recipeName.innerText = recipeList[i].name;
+      recipePic.src = recipeList[i].picture;
+      recipePic.id = recipeList[i].id;
+
+      individualRecipe.appendChild(recipeName);
+      individualRecipe.appendChild(recipePic);
+      recipesDiv.appendChild(individualRecipe)
+      this.contentWrapper.appendChild(recipesDiv);
+    }
+
+  };
 };
+
+const displayRecipePage = function (page) {
+  page = this.id;
+  let pageBody = document.querySelector('.recipe-page');
+  switch (page) {
+    case '1':
+    pageBody.innerHTML = '';
+    pageBody.innerHTML = `<h1>Smoked Salmon Bagel and Avocado!</h1><br><img src=${BagelAvocado} class='recipe-image'>`
+    pageBody.classList = 'recipe-page'
+    break;
+    case '2':
+    pageBody.innerHTML = '';
+    pageBody.innerHTML = `<h1>Gumbo</h1><br><img src=${Gumbo} class='recipe-image'>`
+    pageBody.classList = 'recipe-page'
+    break;
+    case '3':
+    pageBody.innerHTML = '';
+    pageBody.innerHTML = `<h1>Pecan Pie!</h1><br><img src=${Pie} class='recipe-image'>`
+    pageBody.classList = 'recipe-page'
+    break;
+    case '4':
+    pageBody.innerHTML = '';
+    pageBody.innerHTML = `<h1>Gochujang Short Ribs!</h1><br><img src=${ShortRibs} class='recipe-image'>`
+    pageBody.classList = 'recipe-page'
+    break;
+  }
+}
 
 const headerContent = (() => {
   const createHeader = (() => {
@@ -91,6 +158,16 @@ const headerContent = (() => {
 const mainContent = function () {
   let pageBody = document.querySelector('.home-page');
   document.body.appendChild(pageBody);
+  let recipes = [
+    {name: 'Salmon Bagel with Avocado', picture: BagelAvocado, id: 1},
+    {name: 'Gumbo', picture: Gumbo, id: 2},
+    {name: 'Pecan Pie', picture: Pie, id: 3},
+    {name: 'Gochujang Shortribs', picture: ShortRibs, id: 4},
+    {name: 'Salmon Bagel with Avocado', picture: BagelAvocado, id: 1},
+    {name: 'Gumbo', picture: Gumbo, id: 2},
+    {name: 'Pecan Pie', picture: Pie, id: 3},
+    {name: 'Gochujang Shortribs', picture: ShortRibs, id: 4}
+  ]
 
   let homePage = function () {
     let homePage = new Page('Welcome to Chrain\'s Home Recipes!', pageBody, 'home-page', Ingredients);
@@ -98,10 +175,8 @@ const mainContent = function () {
   };
 
   let recipesPage = function () {
-    pageBody.innerHTML = ''
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Recipes Page!';
-    pageBody.appendChild(h1)
+    let contactPage = new Page('The real star of the show. . .the FOOD!', pageBody, 'recipe-page', recipes);
+    contactPage.recipesPageBody();
   };
 
   let contactPage = function () {
