d3975f93b9a1b5ce364b2acf2be2bb2af9d16de9
Blain Crawford <blaincrawford@gmail.com>
2022-03-04 13:53:13 -0500

    running everything through babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,13 @@ module.exports = {
     rules: [
       {
         test: /\.js$/i,
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.css$/i,
